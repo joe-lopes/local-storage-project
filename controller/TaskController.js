66484f8785ed1cf2c3d3aa9ad505f1213410d600
joe.lopes <joelouza@gmail.com>
2021-05-11 12:33:41 -0300

    Main folder structure and JS files

---
+++ controller/TaskController.js
@@ -0,0 +1,13 @@
+class TaskController{
+
+    /*Pegar todos os dados do formulário e imprimir uma string*/
+
+    constructor(){
+
+        taskForm = document.querySelector("#form");
+
+        console.log(taskForm);
+
+    }
+    
+}
---
+++ model/Task.js
@@ -0,0 +1,20 @@
+class Task{
+
+    constructor(){
+        /*
+        ** 1. task name
+        ** 2. task duration
+        ** 3. task cateory
+        ** 4. task kind
+        ** 5. task icon
+        ** 6. task buttons
+        */
+
+        this.name;
+        this.duration;
+        this.category;
+        this.kind;
+        this.icon;
+
+    }
+}
